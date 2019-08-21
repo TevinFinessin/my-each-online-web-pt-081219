076d33948c97f17aab010ed85e6ee23e1b1e3786
Tevin Sherman <tevinsherman@yahoo.com>
2019-08-21 23:41:34 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,3 +3,17 @@ def my_each(words) # put argument(s) here
   my_each(words) do |word|
 end
 end
+#def hello_t(names)
+#  if block_given?
+#i = 0
+
+#while i < names.length
+#yield(names[i])
+#i = i + 1
+#end
+
+#names
+#else 
+ # puts "Hey! No block was given!"
+#end
+#end
