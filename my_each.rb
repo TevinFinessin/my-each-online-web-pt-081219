f177a381e64929dbe9d8995e29cd4b8858b643d1
Tevin Sherman <tevinsherman@yahoo.com>
2019-08-21 23:52:43 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,6 @@
 def my_each(words) # put argument(s) here
   # code here
-  i = 0
   
-  while i < names.length
-  yield(words[i])
-  i = i + 1
-end
-words
 end
 #def hello_t(names)
 #  if block_given?
